6b02b622363b29020804dbeefa6489ba4f24da3f
SarswatiPatil <dipakalagate1991@gmail.com>
2019-11-07 17:29:33 +0530

    Spring IOC Program Completed

---
+++ src/com/bridgelabz/springioc/Airtel.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.springioc;
+
+public class Airtel implements Sim
+{
+
+	@Override
+	public void calling() 
+	{
+		System.out.println("Calling using Airtel sim..");
+	}
+
+	@Override
+	public void data() 
+	{
+		System.out.println("Browsing internet using Airtel sim..");		
+	}
+
+}
---
+++ src/com/bridgelabz/springioc/Jio.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.springioc;
+
+public class Jio implements Sim 
+{
+
+	@Override
+	public void calling()
+	{
+		System.out.println("Calling using Jio sim..");
+		
+	}
+
+	@Override
+	public void data() 
+	{
+		System.out.println("Browsing internet using Jio sim..");
+		
+	}
+
+}
